937dd804a5429b54541f1f642c1b0323f44a42d4
nothern05 <gleb5shimko@gmail.com>
2025-04-14 14:31:43 +0300

    init commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,4 @@
 import os
-import json
 
 from pydantic_settings import BaseSettings
 
@@ -30,36 +29,6 @@ class DBSettings(BaseSettings):
     db_echo: bool = False
 
 
-class CoinMarketCapSettings(BaseSettings):
-    CMC_URL: str = os.environ.get("CMC_URL", "https://pro-api.coinmarketcap.com/v1/cryptocurrency")
-    CMC_API_KEY: str = os.environ.get("CMC_API_KEY", "54feb766-7b6e-4754-8592-b5bc463e2c2a")
-
-
-class ElfaSettings(BaseSettings):
-    ELFA_URL: str = os.environ.get("ELFA_URL", "https://api.elfa.ai/v1")
-    ELFA_API_KEY: str = os.environ.get("ELFA_API_KEY", "elfak_db1eb0fe5cadbee7f798e2f79f5c53b8bfddb7d5")
-
-
-class PerplexitySettings(BaseSettings):
-    PERPLEXITY_URL: str = os.environ.get("PERPLEXITY_URL", "https://api.perplexity.ai")
-    PERPLEXITY_API_KEY: str = os.environ.get("PERPLEXITY_API_KEY",
-                                             "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")
-
-
-class TwitterCredentialsSettings(BaseSettings):
-    accounts: str = os.environ.get("TWITTER_ACCOUNTS", '''[{"username": "GShimko38911", "email": "glib@zpoken.io", "password": "y5iwbGq=/nX:'CL"}, {"username": "GlebShimko", "email": "gleb5shimko@gmail.com", "password": "MU#?<_3Bf2iwU6M"}, {"username": "yur35494", "email": "yura@zpoken.io", "password": "213456qaZ"}]''')
-    ACCOUNTS: list = json.loads(accounts)
-
-
-class LLamaSettings(BaseSettings):
-    LLAMA_URL: str = os.environ.get("LLAMA_URL", "http://195.189.60.154:8000/generate")
-
-
 config = Config()
 db_config = DBSettings()
-cmc_config = CoinMarketCapSettings()
-elfa_config = ElfaSettings()
-perplexity_config = PerplexitySettings()
 redis_config = RedisSettings()
-llama_config = LLamaSettings()
-twitter_account_config = TwitterCredentialsSettings()
--- app/core/modules_factory.py
+++ app/core/modules_factory.py
@@ -1,33 +1,5 @@
 from redis.asyncio import Redis
-from app.core.config import cmc_config, perplexity_config, elfa_config, redis_config, llama_config, \
-    twitter_account_config
-from utils.cmc_driver import CoinMarketCapDriver
-from utils.perplexity_driver import PerplexityDriver
-from utils.elfa_driver import ElfaDriver
-from utils.coingecko_driver import CryptoPriceFetcher
-from utils.llama_driver import LlamaDriver
-from utils.twikit_driver import TwitterScraper
-
-# -------- Initialize CoinMarketCap Driver -----------------
-cmc_driver = CoinMarketCapDriver(
-    base_url=cmc_config.CMC_URL,
-    api_key=cmc_config.CMC_API_KEY
-)
-
-# -------- Initialize Perplexity Driver ---------------------
-perplexity_driver = PerplexityDriver(
-    base_url=perplexity_config.PERPLEXITY_URL,
-    api_key=perplexity_config.PERPLEXITY_API_KEY
-)
-
-# -------- Initialize Elfa Driver ---------------------------
-elfa_driver = ElfaDriver(
-    base_url=elfa_config.ELFA_URL,
-    api_key=elfa_config.ELFA_API_KEY
-)
-
-# ---------- Initialize CoinGecko Driver --------------------
-coin_gecko_driver = CryptoPriceFetcher()
+from app.core.config import redis_config
 
 # -------- Initialize REDIS connection ----------------------
 redis_db = Redis(
@@ -35,12 +7,4 @@ redis_db = Redis(
     port=redis_config.REDIS_PORT,
     username=redis_config.REDIS_USER,
     password=redis_config.REDIS_PASSWORD
-)
-
-# -------- Initialize Llama connection -----------------------
-llama = LlamaDriver(base_url=llama_config.LLAMA_URL)
-
-# --------- Initialize Twitter Scraper -----------------------
-twitter_scraper = TwitterScraper(
-    accounts=twitter_account_config.ACCOUNTS
 )
