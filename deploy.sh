6a5313bd856af75bf2b7cdddea2fbaa4819412ea
nothern05 <gleb5shimko@gmail.com>
2025-06-05 16:13:35 +0300

    update flow deploy

--- deploy.sh
+++ deploy.sh
@@ -33,8 +33,9 @@ if ! echo "$SUPPORTED_ENVS" | grep -wq "$ENV"; then
 fi
 
 # Run docker compose
-systemctl restart docker
+docker stop dev_bot tg_bot
 docker system prune --all --volumes --force
+systemctl restart docker
 docker compose -f .docker/${ENV}-docker-compose.yml --env-file=.envs/.${ENV} up -d --build
 
 # If migration flag is true, run test.sh
