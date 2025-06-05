09f2a6bee1dbd61f79fd72919df7f615d8228862
nothern05 <gleb5shimko@gmail.com>
2025-06-05 12:30:29 +0300

    upds deploy process

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 from types import SimpleNamespace
 from datetime import datetime
 from aiogram.client.default import DefaultBotProperties
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardButton
 from aiogram import Router, F, types, Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
