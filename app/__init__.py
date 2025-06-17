de1f0024e79c7006fef60051e8cc8b040a9eb95a
nothern05 <gleb5shimko@gmail.com>
2025-06-17 13:54:18 +0300

    logging

--- app/__init__.py
+++ app/__init__.py
@@ -72,18 +72,18 @@ async def format_request(request: Request):
 
 
 logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 # add formatter to handlers
 if len(logger.handlers) > 0:
-    logger.handlers[0].setLevel(logging.ERROR)
+    logger.handlers[0].setLevel(logging.INFO)
     logger.handlers[0].setFormatter(formatter)
 else:
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
--- utils/extra.py
+++ utils/extra.py
@@ -28,7 +28,7 @@ def flatten_nested_list(nested_list):
 
 
 def cash_ads():
-    response = requests.post(url="https://affhunter.net/bot/api/v1/creatives/cash_ads")
+    response = requests.get(url="https://affhunter.net/bot/api/v1/creatives/cash_ads")
     if response.status_code == 200:
         return {"ok": True}
     else:
