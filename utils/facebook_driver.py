0d5b2b502fb4bf48178589f709c54756b555529a
nothern05 <gleb5shimko@gmail.com>
2025-06-17 16:22:54 +0300

    upds fb driver

--- utils/facebook_driver.py
+++ utils/facebook_driver.py
@@ -40,11 +40,17 @@ class FacebookAdsLibraryDriver:
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                        "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
             locale="en-US",
-            viewport={"width": 1280, "height": 720}
+            viewport={"width": 1280, "height": 720},
+            storage_state="fb_logged_state.json"
         )
         await context.set_extra_http_headers({
             "Referer": "https://www.facebook.com/",
-            "Accept-Language": "en-US,en;q=0.9"
+            "Accept-Language": "en-US,en;q=0.9",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1"
         })
         return context
 
